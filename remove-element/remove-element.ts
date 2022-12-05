9dfd2789331cc71a73eb92cf2a6b7ef68ff11927
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-05 16:03:40 +0900

    Time: 105 ms, Memory: 46.2 MB - LeetHub

---
+++ remove-element/remove-element.ts
@@ -0,0 +1,14 @@
+function removeElement(nums: number[], val: number): number {
+    let count = 0
+    const copy = [...nums]
+    for(let p = 0; p < copy.length; p++){
+        if(copy[p] === val){
+            nums.splice(p - count,1)
+            count ++
+        }
+    }
+    
+    console.log(nums)
+    
+    return copy.length - count 
+};
