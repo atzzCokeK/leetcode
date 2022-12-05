03590b323234a9be4ed5a2cc86c1da7cf9b0e39c
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-05 15:17:29 +0900

    Time: 116 ms (27.79%), Space: 44.8 MB (19.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,13 +2,17 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    for(let i = 0; i < n; i ++){
-        nums1[i+m] = nums2[i]
-    }
+    const copy = [...nums1]
+    let p1 = 0
+    let p2 = 0
     
-    nums1.sort((a,b) => {if(a > b){
-        return 1
-    }else {
-        return -1
-    }})
+    for (let p = 0; p < m + n; p++){
+        if(p2 >= n || (p1 < m && copy[p1] < nums2[p2])){
+            nums1[p] = copy[p1]
+            p1++
+        } else{
+            nums1[p] = nums2[p2]
+            p2++
+        }
+    }
 };
