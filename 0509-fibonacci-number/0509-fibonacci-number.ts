d83b164a1af6acd6929fdec01d4623506e5ba8ef
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-12 01:24:36 +0900

    Time: 115 ms (40.35%), Space: 43.1 MB (41.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.ts
+++ 0509-fibonacci-number/0509-fibonacci-number.ts
@@ -1,6 +1,9 @@
 function fib(n: number): number {
-    if (n === 0) return 0
-    if (n === 1) return 1
+    let arr = [0, 1]
 
-    return fib(n-1) + fib(n-2)
+    for (let i = 2; i <= n; i++){
+        arr.push(arr[i-2] + arr[i-1])
+    }
+
+    return arr[n]
 };
