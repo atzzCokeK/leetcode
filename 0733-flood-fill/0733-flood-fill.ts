fe3d8f59f7866ac93df3609e364ea51f1a381c1d
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-12 11:22:22 +0900

    Time: 78 ms (96.63%), Space: 45.2 MB (46.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.ts
+++ 0733-flood-fill/0733-flood-fill.ts
@@ -1,5 +1,5 @@
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
-    if (image[sr][sc] === color) return image
+    if (image[sr][sc] == color) return image
 
     const rowLimit = image.length
     const columnLimit = image[0].length
@@ -13,26 +13,26 @@ function floodFill(image: number[][], sr: number, sc: number, color: number): nu
         const row = current[0]
         const column = current[1]
 
-        if (image[row][column] === original) {
+        if (image[row][column] == original) {
             image[row][column] = color
         }
 
-        if (row - 1 >= 0 && image[row - 1][column] === original){
+        if (row - 1 >= 0 && image[row - 1][column] == original){
             image[row - 1][column] = color 
             stack.push([row - 1,column])
         }
         
-        if (row + 1 < rowLimit && image[row + 1][column] === original){
+        if (row + 1 < rowLimit && image[row + 1][column] == original){
             image[row + 1][column] = color 
             stack.push([row + 1, column])
         }
 
-        if (column - 1 >= 0 && image[row][column - 1] === original){
+        if (column - 1 >= 0 && image[row][column - 1] == original){
             image[row][column - 1] = color 
             stack.push([row, column - 1])
         }
 
-        if (column + 1 < columnLimit && image[row][column + 1] === original){
+        if (column + 1 < columnLimit && image[row][column + 1] == original){
             image[row][column + 1] = color
             stack.push([row, column + 1])
         }
