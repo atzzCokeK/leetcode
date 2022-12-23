ba9a68e114aef94435ff70248b0bc97cc8695ff4
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-23 20:05:19 +0900

    Time: 142 ms (34.07%), Space: 45.9 MB (41.08%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.ts
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.ts
@@ -1,31 +1,26 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
  * }
  */
 
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
-var detectCycle = (head) => {
-    if (!head || !head.next){
-        return null
+function detectCycle(head: ListNode | null): ListNode | null {
+    const arr = []
+    
+    while(head !== null){
+        if (arr.includes(head)){
+            return head
+        }
+        
+        arr.push(head)
+        head = head.next
     }
     
-    const set = new Set()
-    let tmp = head
-    while(tmp){
-        if (set.has(tmp)){
-            return tmp
-        } else {
-            set.add(tmp)
-            tmp = tmp.next
-        }
-    }
-
     return null
 };
