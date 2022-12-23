aa739ed05f1a5d47b6a7347087a55330b6193d92
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2022-12-23 20:42:45 +0900

    Time: 96 ms, Memory: 42 MB - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const answers = []
+    const func = (node) => {
+        if (!node) return
+        
+        func(node.left)
+        answers.push(node.val)
+        func(node.right)
+    }
+    
+    func(root)
+    
+    return answers
+};
