9def94ca3e8845f32b1fe421429e532713b848be
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2023-04-01 21:53:29 +0900

    Time: 58 ms (97.08%), Space: 45.2 MB (91.99%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -1,12 +1,7 @@
 function singleNumber(nums: number[]): number {
-    const count = {};
-    for (let i = 0; i < nums.length; i++){
-        count[nums[i]] = count[nums[i]] ? 2 : 1;
+    let single = 0;
+    for (let num of nums) {
+        single ^= num;
     }
-    
-    for(const [key, value] of Object.entries(count)){
-        if(value === 1){
-            return Number(key)
-        }
-    }
-};
+    return single;
+}
