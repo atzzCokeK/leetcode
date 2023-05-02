c0af3fa08527ed6bfaed521d009ad42566237476
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2023-05-03 06:48:21 +0900

    Time: 74 ms (99.11%), Space: 59.5 MB (5.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -0,0 +1,13 @@
+function containsDuplicate(nums: number[]): boolean {
+    const map = new Map()
+    
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            return true
+        } else {
+            map.set(nums[i],"")
+        }
+    }
+    
+    return false
+};
