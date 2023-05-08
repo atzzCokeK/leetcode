020bb05bd0c05eeafd6244a0db390a612fc2c04a
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2023-05-08 12:59:49 +0900

    Time: 70 ms (10.39%), Space: 43.3 MB (31.17%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.ts
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -1,13 +1,20 @@
-function once(fn) {
-  let called = false;
-  let result;
-  return function(...args) {
-    if (!called) {
-      called = true;
-      result = fn(...args);
-      return result
-    } else {
-      return undefined;
-    }
-  }
+function once<T extends (...args: any[]) => any>(fn: T): 
+ ((...args: Parameters<T>) => ReturnType<T> | undefined) {
+  let isDone = false
+  return function (...args) {
+      if(isDone){
+          return undefined
+      }
+      
+      isDone = true
+      return fn(...args)
+  };
 }
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
