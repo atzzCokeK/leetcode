aef1401accc059a69622b2df1ea926b4a75fe73a
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2023-05-10 23:19:44 +0900

    Time: 68 ms (79.66%), Space: 49.1 MB (56.50%) - LeetHub

--- 2632-curry/2632-curry.ts
+++ 2632-curry/2632-curry.ts
@@ -1,12 +1,10 @@
 function curry(fn) {
   return function curried(...args) {
-    console.log("fn",fn.length)
-    console.log(this)
     if (args.length >= fn.length) {
-      return fn.apply(null, args);
+      return fn(...args);
     } else {
       return function(...moreArgs) {
-        return curried.apply(null, args.concat(moreArgs));
+        return curried(...args.concat(moreArgs));
       }
     }
   }
