907adc6bc38271cedcc2ea61acb64debf99b98b2
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2026-01-13 23:42:04 +0900

    Time: 2 ms (45.16%), Space: 56.3 MB (69.38%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,10 +1,9 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
     const setNums1 = new Set(nums1)
     const setNums2 = new Set(nums2)
-    const iterator = setNums1.entries();
     const ans = []
 
-    for (const [x,y] of iterator) {
+    for (const x of setNums1) {
         if (!setNums2.has(x)){
             continue
         }
