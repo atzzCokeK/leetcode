ccde37f7a88415a974c3546b5eaba4433e8e1a68
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2026-01-16 23:37:10 +0900

    Time: 512 ms (5.07%), Space: 80.3 MB (39.51%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -0,0 +1,16 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    const ans = Array(temperatures.length).fill(0)
+    const stack = []
+
+    for (let i = 0; i < temperatures.length; i++){
+
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const top = stack.pop()
+            ans[top] = i - top
+        }
+
+        stack.push([i])
+    }
+
+    return ans
+};
